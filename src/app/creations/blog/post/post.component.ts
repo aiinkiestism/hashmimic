1897a1f38aa9cfb3fd8539f0c5e0a66b7a7276ab
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2020-06-07 00:22:56 +0900

    complete the post page temporarily without some functions

--- src/app/creations/blog/post/post.component.ts
+++ src/app/creations/blog/post/post.component.ts
@@ -11,6 +11,9 @@ import { Entry } from 'contentful';
 export class PostComponent implements OnInit {
 
   post: Entry<any>;
+  categories: Entry<any>[] = [];
+  category: Entry<any>;
+  pubCategoryId;
 
   constructor(
     private route: ActivatedRoute,
@@ -23,12 +26,28 @@ export class PostComponent implements OnInit {
     this.contentfulService.getPost(postId)
       .then((post) => {
         this.post = post;
-        console.log(this.post);
+        // console.log(this.post);
       });
+
+    const categoryId = this.route.snapshot.paramMap.get('id');
+    this.pubCategoryId = categoryId;
+    this.contentfulService.getCategory(categoryId)
+      .then((category) => {
+        this.category = category;
+        // console.log(category);
+      });
+
+    this.contentfulService.getCategories()
+    .then(categories => this.categories = categories);
+
   }
 
   goToBlog() {
     this.router.navigate(['/blog']);
+  }
+
+  goToCategory(categoryId) {
+    this.router.navigate(['/creations/blog/category', categoryId]);
   }
 
 }
