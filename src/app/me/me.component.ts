f53c4c9df0f0571dcf42c6009b62edd6de9cff1d
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2020-06-20 12:17:15 +0900

    update and complete me and renovate header

--- src/app/me/me.component.ts
+++ src/app/me/me.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-me',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeComponent implements OnInit {
 
-  constructor() { }
+  main = document.getElementById("main");
+
+  constructor(
+    private router: Router
+  ) {
+    this.router.events
+      .pipe(filter(e => e instanceof NavigationStart))
+      .subscribe(() => this.main.setAttribute('style', "background: #000; padding-bottom: 0px;") );
+  }
 
   ngOnInit(): void {
+    this.main.setAttribute('style', "background: linear-gradient(#000 0%, #536AFC 35%, #FF007B 100%); padding-bottom: 1px;");
   }
 
 }
